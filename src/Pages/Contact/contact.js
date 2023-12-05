e0869e2796b71e4f721cab17483a1430c2c5eb4b
rezy3312 <rezy3312@yahoo.com>
2023-12-05 15:04:52 -0600

    Updateing codes

--- src/Pages/Contact/contact.js
+++ src/Pages/Contact/contact.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "../Contact/contact.css";
+
 
 const contact = () => {
   return (
-    <h1>This is the contact page</h1>
-  )
-}
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <p>
+        Have questions, suggestions, or just want to say hello? We'd love to
+        hear from you! Feel free to reach out using the contact information
+        below.
+      </p>
+      <div className="contact-details">
+        <p>Email: info@pizzaparadise.com</p>
+        <p>Phone: 123-456-7890</p>
+        <p>Address: 123 Pizza Street, Cityville, State 12345</p>
+      </div>
+    </div>
+  );
+};
 
-export default contact
+export default contact;
--- src/Pages/Menu/menu.js
+++ src/Pages/Menu/menu.js
@@ -1,9 +1,55 @@
 import React from 'react';
 
+import '../Menu/menu.css';
+
 const menu = () => {
   return (
-    <h1>Menu site</h1>
-  )
-}
+    <div className="menu-container">
+      <div className="menu-buttons">
+        <button onClick={() => scrollToSection('coupons')}>Coupons</button>
+        <button onClick={() => scrollToSection('desserts')}>Desserts</button>
+        <button onClick={() => scrollToSection('pizza')}>Pizza</button>
+      </div>
 
-export default menu
+      <section id="coupons" className="menu-section">
+        <h2>Coupons</h2>
+        {renderMenuItems('Coupon')}
+      </section>
+
+      <section id="desserts" className="menu-section">
+        <h2>Desserts</h2>
+        {renderMenuItems('Dessert')}
+      </section>
+
+      <section id="pizza" className="menu-section">
+        <h2>Pizza</h2>
+        {renderMenuItems('Pizza')}
+      </section>
+    </div>
+  );
+};
+
+const renderMenuItems = (category) => {
+  const items = Array.from({ length: 8 }, (_, index) => index + 1);
+
+  return (
+    <div className="menu-items">
+      {items.map((item) => (
+        <div key={item} className="menu-item">
+          <img src={`images/${category}${item}.jpg`} alt={`${category} ${item}`} />
+          <span>{`${category} ${item}`}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const scrollToSection = (sectionId) => {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+export default menu;
+
